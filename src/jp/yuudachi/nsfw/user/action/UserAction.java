b6dc62b96bc9709c6c56d45a36bcec3c9467c066
Dominic <wobuxindelete@qq.com>
2017-01-03 11:15:44 +0800

    [201701031114]Add methods of headImg
    Add/update and save methods of headImg

--- src/jp/yuudachi/nsfw/user/action/UserAction.java
+++ src/jp/yuudachi/nsfw/user/action/UserAction.java
@@ -1,8 +1,15 @@
 package jp.yuudachi.nsfw.user.action;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletContext;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.struts2.ServletActionContext;
 
 import jp.yuudachi.nsfw.user.entity.User;
 import jp.yuudachi.nsfw.user.service.UserService;
@@ -16,6 +23,9 @@ public class UserAction extends ActionSupport {
 	private List<User> userList;
 	private User user;
 	private String[] selectedRow;
+	private File headImg;
+	private String headImgContentType;
+	private String headImgFileName;
 	
 	//列表页面
 	public String listUI(){
@@ -28,8 +38,23 @@ public class UserAction extends ActionSupport {
 	}
 	//保存新增
 	public String add(){
-		if(user != null){
-			userService.save(user);
+		try {
+			if(user != null){
+				//处理头像
+				if(headImg != null){
+					//1.保存到upload/user
+					//获取保存路径的绝对地址
+					String filePath = ServletActionContext.getServletContext().getRealPath("upload/user");
+					String fileName = UUID.randomUUID().toString().replaceAll("-", "") + headImgFileName.substring(headImgFileName.lastIndexOf("."));
+					//复制文件
+					FileUtils.copyFile(headImg, new File(filePath,fileName));
+					//2.设置用户头像路径
+					user.setHeadImg("user/" + fileName);
+				}
+				userService.save(user);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		return "list";
 	}
@@ -42,8 +67,23 @@ public class UserAction extends ActionSupport {
 	}
 	//保存编辑
 	public String edit(){
-		if(user != null){
-			userService.update(user);
+		try {
+			if(user != null){
+				//处理头像
+				if(headImg != null){
+					//1.保存到upload/user
+					//获取保存路径的绝对地址
+					String filePath = ServletActionContext.getServletContext().getRealPath("upload/user");
+					String fileName = UUID.randomUUID().toString().replaceAll("-", "") + headImgFileName.substring(headImgFileName.lastIndexOf("."));
+					//复制文件
+					FileUtils.copyFile(headImg, new File(filePath,fileName));
+					//2.设置用户头像路径
+					user.setHeadImg("user/" + fileName);
+				}
+				userService.update(user);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		return "list";
 	}
@@ -63,6 +103,7 @@ public class UserAction extends ActionSupport {
 		}
 		return "list";
 	}
+	
 	public UserService getUserService() {
 		return userService;
 	}
@@ -86,5 +127,23 @@ public class UserAction extends ActionSupport {
 	}
 	public void setSelectedRow(String[] selectedRow) {
 		this.selectedRow = selectedRow;
+	}
+	public File getHeadImg() {
+		return headImg;
+	}
+	public void setHeadImg(File headImg) {
+		this.headImg = headImg;
+	}
+	public String getHeadImgContentType() {
+		return headImgContentType;
+	}
+	public void setHeadImgContentType(String headImgContentType) {
+		this.headImgContentType = headImgContentType;
+	}
+	public String getHeadImgFileName() {
+		return headImgFileName;
+	}
+	public void setHeadImgFileName(String headImgFileName) {
+		this.headImgFileName = headImgFileName;
 	}
 }
