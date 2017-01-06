ca17a6eef0d075fb4bf28ea7f667fed43763340e
Dominic <wobuxindelete@qq.com>
2017-01-06 16:30:46 +0800

    [201701061629]Add home page controller(not finished)
    增加主页控制和显示模块（未加入整合逻辑）

---
+++ src/jp/yuudachi/home/action/HomeAction.java
@@ -0,0 +1,12 @@
+package jp.yuudachi.home.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HomeAction extends ActionSupport{
+	
+	//跳转到首页
+	public String execute(){
+		return "home";
+	}
+	
+}
---
+++ src/jp/yuudachi/nsfw/home/action/HomeAction.java
@@ -0,0 +1,9 @@
+package jp.yuudachi.nsfw.home.action;
+
+import jp.yuudachi.core.action.BaseAction;
+
+public class HomeAction extends BaseAction{
+	
+	
+	
+}
